8cb528112867db69060851db225bff15b94bb7d4
theVictorDara <101578402+theVictorDara@users.noreply.github.com>
2022-06-23 10:44:09 +0100

    Added task submission.

---
+++ intro_to_js.js
@@ -0,0 +1,7 @@
+// JavaScript Code that prints out my name, height, and country on the screen.
+
+console.log("Name: Victor Durosaro");
+
+console.log("Height: 1.6m");
+
+console.log("Country: Nigeria");
